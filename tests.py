9645f778c1c57df264f3d5ff0839be7837b1482a
Vbrawl <greninjagr@gmail.com>
2023-07-31 14:12:12 +0300

    Added testcase for Project class.

--- tests.py
+++ tests.py
@@ -41,5 +41,45 @@ class Test_Utils(unittest.TestCase):
 
 
 
+import Project
+class Test_Project(unittest.TestCase):
+    def test_import_project(self):
+        dir1 = tempfile.mkdtemp("1", "project")
+        dir2 = tempfile.mkdtemp("2", "project")
+
+        project_json_1 = {
+            "project_name": "TestProject1",
+            "project_version": "1.6.5",
+            "project_url": "https://github.com/",
+            "project_requirements": {},
+            "project_library_directory": "pm_library"
+        }
+
+        project_json = open(os.path.join(dir1, "project.json"), 'w')
+        json.dump(project_json_1, project_json)
+        project_json.close()
+
+        project_json_2 = project_json_1
+        project_json_2["project_name"] = "TestProject2"
+
+        project_json = open(os.path.join(dir2, "project.json"), "w")
+        json.dump(project_json_2, project_json)
+        project_json.close()
+
+        p1 = Project.Project(dir1)
+        p2 = Project.Project(dir2)
+
+        p2.import_project(p1)
+
+        dir3 = os.path.join(p2.path, p2.library_directory, p1.name)
+        self.assertTrue(os.path.isdir(dir3), "Project was not imported at all")
+        p3 = Project.Project(dir3)
+
+        self.assertEqual(p1.name, p3.name)
+        self.assertEqual(p1.version, p3.version)
+        self.assertEqual(p1.url, p3.url)
+        self.assertEqual(p1.requirements, p3.requirements)
+        self.assertEqual(p1.library_directory, p3.library_directory)
+
 if __name__ == "__main__":
     unittest.main()
