b3ffa88690807336ba772f4a7da97980ace965d7
Vbrawl <greninjagr@gmail.com>
2023-08-12 10:34:44 +0300

    Added support for multiple platforms.

--- src/Config.py
+++ src/Config.py
@@ -1,9 +1,21 @@
-import os
+import os, sys
+
+app_name = "PHPPM"
+
+if os.name == "nt":
+    app_directory = os.path.join(os.getenv("APPDATA"), app_name)
+elif os.getenv("TERMUX_VERSION", None) is not None:
+    app_directory = os.path.join("/data/data/com.termux/files/usr/etc", app_name)
+else:
+    app_directory = os.path.join("/etc", app_name)
+
+if not os.path.exists(app_directory):
+    os.mkdir(app_directory)
 
 
 
 class Config:
-    library_path = "C:/Users/Jim/Desktop/php_pm/library"
+    library_path = os.path.join(app_directory, "pm_library")
 
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
