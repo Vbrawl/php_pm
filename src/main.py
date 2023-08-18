d600ba6a13b818fb437d504d00aafafe81ed8797
Vbrawl <greninjagr@gmail.com>
2023-08-18 15:10:26 +0300

    Added argument parsing. Modified initialize_project function to create library folder and relocation configs. Added requirements.txt

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from Project import *
 from Config import *
 import os
+import argparse
 
 config = Config()
 
@@ -14,10 +15,31 @@ class Main():
     def initialize_project(self):
         proj = Project(os.getcwd())
         projJs = os.path.join(proj.path, proj.project_json_filename)
-        if not os.path.exists(projJs):
+        if not os.path.isfile(projJs):
             proj.save(projJs)
+        projLibrary = os.path.join(proj.path, proj.library_directory)
+        if not os.path.isdir(projLibrary):
+            proj.clear_library_folder()
+        projRelocation = os.path.join(proj.path, proj.relocation_config)
+        if not os.path.isfile(projRelocation):
+            proj.add_root_relocation()
 
 
 if __name__ == "__main__":
     m = Main(config)
-    m.initialize_project()
+
+    ACTIONS = {
+        "init": m.initialize_project,
+    }
+
+
+    parser = argparse.ArgumentParser(app_name)
+    parser.add_argument("action", action="store", help="Available Actions: " + ', '.join(ACTIONS.keys()))
+
+    args = parser.parse_args()
+
+
+
+    action:str = args.action
+    if action in ACTIONS:
+        ACTIONS[action]()
